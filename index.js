2f63e03622a8f09183f6d11f041a12fec8d48625
Kim <rodrigkim@gmail.com>
2022-09-13 12:26:31 -0400

    added form

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const recipeImage = document.getElementById("detail-image");
 const recipeName = document.getElementById("directions-header");
 const recipeInstructions = document.getElementById("directions");
 const recipeIngredients = document.getElementById("ingredients");
+const recipeBanner = document.querySelector('#recipe-banner')
+const API_KEY = 9973533
 
 // load random recipe at first
 function loadRandom(){
@@ -12,20 +14,18 @@ function loadRandom(){
         const mealImage = recipe.meals[0].strMealThumb;
         const instructions = recipe.meals[0].strInstructions;
         //there are no more than 20 ingredients in each recipe
-        for (let i = 0; i < 20; i++){
-            let ingredientName = recipe.meals[0][`strIngredient${i+1}`]
-            let ingredientMeasure = recipe.meals[0][`strMeasure${i+1}`]
+        for (let i = 1; i < 21; i++){
+            let ingredientName = recipe.meals[0][`strIngredient${i}`]
+            let ingredientMeasure = recipe.meals[0][`strMeasure${i}`]
             let ingredientEntry = ingredientMeasure + ' ' + ingredientName
             const li = document.createElement("li")
-            if (ingredientEntry != "" && ingredientEntry != " "){
+            if (ingredientName != ""){
                 li.textContent = ingredientEntry
-                console.log(ingredientEntry)
                 recipeIngredients.append(li)
-            }   
+            }  
         }
         recipeImage.src = mealImage;
         recipeName.textContent = `Here's an idea: ${mealName}`;
-        console.log(instructions)
         recipeInstructions.textContent = instructions;
     })
 }
@@ -40,7 +40,6 @@ fetch('https://www.themealdb.com/api/json/v2/9973533/latest.php')
 }))
 
 function renderRecipeBar(meal) {
-    const recipeBanner = document.querySelector('#recipe-banner')
     const img = document.createElement('img')
     img.src = meal.strMealThumb
     recipeBanner.append(img)
@@ -50,30 +49,53 @@ function renderRecipeBar(meal) {
         recipeInstructions.textContent = meal.strInstructions
         recipeName.textContent = meal.strMeal
         recipeIngredients.innerHTML = ""
-        for (let i = 0; i < 20; i++){
-            let ingredientName = meal[`strIngredient${i+1}`]
-            let ingredientMeasure = meal[`strMeasure${i+1}`]
+        for (let i = 1; i < 21; i++){
+            let ingredientName = meal[`strIngredient${i}`]
+            let ingredientMeasure = meal[`strMeasure${i}`]
             let ingredientEntry = ingredientMeasure + ' ' + ingredientName
             const li = document.createElement("li")
-            console.log(ingredientEntry)
-            if (ingredientEntry != "" && ingredientEntry != " "){
+            if (ingredientName != ""){
                 li.textContent = ingredientEntry
-                console.log(ingredientEntry)
                 recipeIngredients.append(li)
             }   
         }
     })
 }
 
+function textify(string){
+    let newString = "";
+    for (letter in string){
+        if (string[letter] == " "){
+            newString = newString + "_"
+        } 
+        else{
+            newString = newString + string[letter].toLowerCase()
+        }
+    }
+    return newString
+}
+
+function clearBar(){
+    recipeBanner.innerHTML = ""
+}
 
 function handleForm() {
     const form = document.querySelector('#ingredient-form')
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-        const firstIng = e.target["first"].value
-        const secondIng = e.target["second"].value
-        const thirdIng = e.target["third"].value
+        const firstIng = textify(e.target["first"].value)
+        const secondIng = textify(e.target["second"].value)
 
-        // const newRecipe = 
+        clearBar();
+
+        fetch(`https://www.themealdb.com/api/json/v2/9973533/filter.php?i=${firstIng},${secondIng}`)
+        .then(res => res.json())
+        .then(data => data.meals.forEach((meal) => {
+            renderRecipeBar(meal)
+        } 
+        ))
     })
 }
+
+
+handleForm()
